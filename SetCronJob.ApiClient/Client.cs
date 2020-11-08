34b700260bd4bf9b4099e2f9f8aa5710bcffe6a4
Adam O'Neil <adamosoftware@gmail.com>
2020-11-08 12:29:21 -0500

    exception factory

--- SetCronJob.ApiClient/Client.cs
+++ SetCronJob.ApiClient/Client.cs
@@ -1,6 +1,9 @@
 ﻿using Refit;
 using SetCronJob.ApiClient.Interfaces;
 using SetCronJob.ApiClient.Models;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SetCronJob.ApiClient
@@ -13,11 +16,33 @@ namespace SetCronJob.ApiClient
         public Client(string token)
         {
             _token = token;
-            _api = RestService.For<ISetCronJobApi>("https://www.setcronjob.com");
+            _api = RestService.For<ISetCronJobApi>("https://www.setcronjob.com", new RefitSettings()
+            {
+                ExceptionFactory = async (response) =>
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var error = JsonSerializer.Deserialize<Error>(json);
+                    return new Exception(error.ToString());
+                }
+            });
         }
 
         public async Task<CronJob> CreateJobAsync(CronJob cronJob) => await _api.CreateJobAsync(_token, cronJob);
 
         public async Task DeleteJob(int id) => await _api.DeleteJobAsync(_token, id);
+
+        private class Error
+        {
+            [JsonPropertyName("status")]
+            public string Status { get; set; }
+
+            [JsonPropertyName("code")]
+            public int Code { get; set; }
+
+            [JsonPropertyName("message")]
+            public string Message { get; set; }
+
+            public override string ToString() => $"{Message} (code {Code})";
+        }
     }
 }
---
+++ SetCronJob.ApiClient/Models/SetCronJobPost.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SetCronJob.ApiClient.Models
+{
+    /// <summary>
+    /// SetCronJob requires a token property with all posts, so I inline it into all objects 
+    /// </summary>
+    public class SetCronJobPost
+    {
+        [JsonPropertyName("token")]
+        public string Token { get;  set; }
+    }
+}
